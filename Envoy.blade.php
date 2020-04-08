2cf8a7967451d18d5e6d538e820fbe3c193f041f
mobin <mobinyazdanparast9@gmail.com>
2020-04-08 12:04:50 +0430

    add envoy config

---
+++ Envoy.blade.php
@@ -0,0 +1,29 @@
+@servers(['web' => ['root@78.47.116.174']])
+
+@setup
+$path = "/home/new_pilo";
+@endsetup
+
+@task('deploy')
+cd {{$path}}
+
+php artisan down
+
+
+@if ($branch)
+    git pull origin {{$branch}}
+@else
+    git pull origin master
+@endif
+
+@if($composer)
+    cd {{$path}}
+    composer update
+@endif
+
+@if($migrate)
+    cd {{$path}}
+    php artisan migrate
+@endif
+
+php artisan cache:clear
