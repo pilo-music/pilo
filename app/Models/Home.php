3405c4e1dddd17ff37bf20c9bc5cd140aab39546
mobin <mobinyazdanparast9@gmail.com>
2020-04-11 18:55:46 +0430

    fix bugs

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -22,8 +22,8 @@ class Home extends Model
     const TYPE_PLAYLIST_GRID = 8;
     const TYPE_MUSIC_GRID = 9;
     const TYPE_MUSIC_TRENDING = 10;
-    const TYPE_VIDEOS = 10;
-    const TYPE_MUSIC_VERTICAL = 11;
+    const TYPE_VIDEOS = 11;
+    const TYPE_MUSIC_VERTICAL = 12;
 
     const SORT_LATEST = "latest";
     const SORT_BEST = "best";
