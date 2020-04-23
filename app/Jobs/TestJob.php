c074bcfabeb2ea98b08e3f33d31577a417557b70
mobin <mobinyazdanparast9@gmail.com>
2020-04-23 15:36:32 +0430

    test some stuff

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\AdminMessages;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -32,6 +33,19 @@ class TestJob implements ShouldQueue
     {
         $command = "/usr/local/bin/deez-dw -l https://open.spotify.com/track/0SxjNrhMwarsbcgEy0pavJ -o /home/1587560780507190 /home/deezloader/setting.ini";
         $command = new Command($command);
-        $command->execute();
+        if (!$command->execute()) {
+            AdminMessages::create([
+                'type' => AdminMessages::TYPE_ERROR,
+                'user_id' => 1,
+                'message' => $command->getError()
+            ]);
+        } else {
+            AdminMessages::create([
+                'type' => AdminMessages::TYPE_ERROR,
+                'user_id' => 1,
+                'message' => "ok"
+            ]);
+        }
+
     }
 }
