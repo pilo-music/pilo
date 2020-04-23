70be30ae526771b0b146162a639485001236037d
mobin <mobinyazdanparast9@gmail.com>
2020-04-23 15:22:57 +0430

    test some stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,15 @@ class HomeController extends Controller
     {
         return view('welcome');
     }
+
+    public function test()
+    {
+        echo 'ok';
+        $command = "deez-dw -l https://open.spotify.com/track/0SxjNrhMwarsbcgEy0pavJ -o /home /home/deezloader/setting.ini";
+        $command = new Command($command);
+        if (!$command->execute()) {
+            dd($command->getOutput());
+        }
+        dd($command->getOutput());
+    }
 }
