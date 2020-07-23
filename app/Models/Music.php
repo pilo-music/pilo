5bd0cf96505df851cf001515ae0c4d592553a483
mobin <mobin@gmail.com>
2020-07-23 18:58:07 +0430

    adding elastic search

--- app/Models/Music.php
+++ app/Models/Music.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 
 class Music extends Model
 {
-    use Notifiable;
+    use Notifiable, Searchable;
 
     protected $table = "musics";
 
