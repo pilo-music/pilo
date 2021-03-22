c58b28e618a866d308759083e97b3b11d2c32d2f
mobin <mobinyazdanparast9@gmail.com>
2021-03-22 23:01:34 +0430

    add login with google

--- config/pilo.php
+++ config/pilo.php
@@ -2,6 +2,6 @@
 
 return [
     'download_host_url' => 'https://dl.pilo.app',
-    'google_login_client_id' => env('GOOGLE_DRIVE_CLIENT_ID'),
+    'google_login_client_id' => env('GOOGLE_LOGIN_CLIENT_ID'),
     'google_login_client_secret' => env('GOOGLE_CLIENT_SECRET')
 ];
