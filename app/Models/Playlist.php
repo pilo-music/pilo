2027f99dde93d83262fb8938a2ca39477d093e6e
mobin <mobinyazdanparast9@gmail.com>
2021-07-13 20:01:27 +0430

    add new meilisearch driver

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -46,48 +46,4 @@ class Playlist extends Model
     {
         return $this->morphToMany(Artist::class, 'artistable');
     }
-
-    protected $indexSettings = [
-        'analysis' => [
-            'char_filter' => [
-                'replace' => [
-                    'type' => 'mapping',
-                    'mappings' => [
-                        '&=> and '
-                    ],
-                ],
-            ],
-            'filter' => [
-                'word_delimiter' => [
-                    'type' => 'word_delimiter',
-                    'split_on_numerics' => false,
-                    'split_on_case_change' => true,
-                    'generate_word_parts' => true,
-                    'generate_number_parts' => true,
-                    'catenate_all' => true,
-                    'preserve_original' => true,
-                    'catenate_numbers' => true,
-                ]
-            ],
-            'analyzer' => [
-                'default' => [
-                    'type' => 'custom',
-                    'char_filter' => [
-                        'html_strip',
-                        'replace',
-                    ],
-                    'tokenizer' => 'whitespace',
-                    'filter' => [
-                        'lowercase',
-                        'word_delimiter',
-                    ],
-                ],
-            ],
-        ],
-    ];
-
-    public function getIndexName()
-    {
-        return 'playlists';
-    }
 }
