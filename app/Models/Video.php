f0bd9b3a5f93177054cd88c900be04ce55a9eca5
mobin <mobinyazdanparast9@gmail.com>
2021-07-13 20:23:05 +0430

    add new meilisearch driver

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -56,4 +56,48 @@ class Video extends Model
     {
         return $this->morphMany(Source::class, 'sourceable');
     }
+
+    protected $indexSettings = [
+        'analysis' => [
+            'char_filter' => [
+                'replace' => [
+                    'type' => 'mapping',
+                    'mappings' => [
+                        '&=> and '
+                    ],
+                ],
+            ],
+            'filter' => [
+                'word_delimiter' => [
+                    'type' => 'word_delimiter',
+                    'split_on_numerics' => false,
+                    'split_on_case_change' => true,
+                    'generate_word_parts' => true,
+                    'generate_number_parts' => true,
+                    'catenate_all' => true,
+                    'preserve_original' => true,
+                    'catenate_numbers' => true,
+                ]
+            ],
+            'analyzer' => [
+                'default' => [
+                    'type' => 'custom',
+                    'char_filter' => [
+                        'html_strip',
+                        'replace',
+                    ],
+                    'tokenizer' => 'whitespace',
+                    'filter' => [
+                        'lowercase',
+                        'word_delimiter',
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    public function getIndexName()
+    {
+        return 'videos';
+    }
 }
